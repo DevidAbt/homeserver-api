9b7e5c5af93f3aa4cb68b046bb38ba21856c3a1c
DevidAbt <abt.devid@gmail.com>
2022-02-24 01:52:50 +0100

    Implemented temp logic in js

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,24 @@
 import { execSync } from "child_process";
 
-const temp = () => JSON.parse(Buffer.from(execSync("temp")).toString());
+const temp = () => {
+  const sensors = JSON.parse(Buffer.from(execSync("sensors -j")).toString());
+
+  const hddtemp: any = {};
+  ["sda", "sdb", "sdc", "sdd"].forEach((x) => {
+    const result = Buffer.from(execSync(`sudo hddtemp /dev/${x}`)).toString();
+    if (result) {
+      const array = result.split(":");
+      hddtemp[array[1].trim()] = parseInt(array[2].replace("°C", ""), 10);
+    }
+  });
+
+  return {
+    fan2: sensors["nct6791-isa-0290"].fan2.fan2_input,
+    SYSTIN: sensors["nct6791-isa-0290"].SYSTIN.temp1_input,
+    CPUTIN: sensors["nct6791-isa-0290"].CPUTIN.temp2_input,
+    GPU: sensors["nouveau-pci-0100"].temp1.temp1_input,
+    ...hddtemp,
+  };
+};
 
 export default { temp };
