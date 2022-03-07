88fcc0c364686a1916c1c4e9c62539e8271f5e89
DevidAbt <abt.devid@gmail.com>
2022-03-08 00:28:53 +0100

    Added sde and ?. operators

--- src/service.ts
+++ src/service.ts
@@ -4,7 +4,7 @@ const temp = () => {
   const sensors = JSON.parse(Buffer.from(execSync("sensors -j")).toString());
 
   const hddtemp: any = {};
-  ["sda", "sdb", "sdc", "sdd"].forEach((x) => {
+  ["sda", "sdb", "sdc", "sdd", "sde"].forEach((x) => {
     const result = Buffer.from(execSync(`sudo hddtemp /dev/${x}`)).toString();
     if (result) {
       const array = result.split(":");
@@ -13,10 +13,10 @@ const temp = () => {
   });
 
   return {
-    fan2: sensors["nct6791-isa-0290"].fan2?.fan2_input,
-    SYSTIN: sensors["nct6791-isa-0290"].SYSTIN?.temp1_input,
-    CPUTIN: sensors["nct6791-isa-0290"].CPUTIN?.temp2_input,
-    GPU: sensors["nouveau-pci-0100"].temp1?.temp1_input,
+    fan2: sensors["nct6791-isa-0290"]?.fan2?.fan2_input,
+    SYSTIN: sensors["nct6791-isa-0290"]?.SYSTIN?.temp1_input,
+    CPUTIN: sensors["nct6791-isa-0290"]?.CPUTIN?.temp2_input,
+    GPU: sensors["nouveau-pci-0100"]?.temp1?.temp1_input,
     disks: hddtemp,
   };
 };
